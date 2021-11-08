30022f82b521fe989a88fb8dfdd2b3cf401ab73f
Mohammed-Razzak <92763175+Mohammed-Razzak@users.noreply.github.com>
2021-11-08 15:58:40 +0000

    added remote mysql connection

--- config.php
+++ config.php
@@ -1,9 +1,15 @@
 <?php 
+//development connection
+// $server = "localhost";
+// $user = "root";
+// $pass = "";
+// $database = "login_register";
 
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "login_register";
+//Remote database connection
+$server = "remotemysql.com";
+$user = "nHA3oxl9X1";
+$pass = "oy4DxxGTUk";
+$database = "nHA3oxl9X1";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
 
