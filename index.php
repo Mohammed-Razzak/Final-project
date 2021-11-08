93932f4a8af529c393d32f6c14d2a41c8efa202a
Mohammed-Razzak <92763175+Mohammed-Razzak@users.noreply.github.com>
2021-11-08 19:21:46 +0000

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (isset($_POST['submit'])) {
 
 	<link rel="stylesheet" type="text/css" href="style.css">
 
-	<title>Login Form - Pure Coding</title>
+	<title>Login to Planfit!</title>
 </head>
 <body id="login-body">
 	<div class="container-login">
