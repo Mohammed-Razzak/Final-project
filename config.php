24d8a3395f650afbc87ffd1b1cc227fe51edeb92
Mohammed-Razzak <92763175+Mohammed-Razzak@users.noreply.github.com>
2022-01-26 17:03:41 +0000

    Update config.php

--- config.php
+++ config.php
@@ -8,11 +8,6 @@ $user = "root";
 $pass = "";
 $database = "login_register";
 
-// Remote database connection
-// $server = "remotemysql.com";
-// $user = "nHA3oxl9X1";
-// $pass = "oy4DxxGTUk";
-// $database = "nHA3oxl9X1";
 
 
 $conn = mysqli_connect($server, $user, $pass, $database);
@@ -22,3 +17,11 @@ if (!$conn) {
 }
 
 ?>
+
+
+
+<!-- // Remote database connection
+// $server = "remotemysql.com";
+// $user = "nHA3oxl9X1";
+// $pass = "oy4DxxGTUk";
+// $database = "nHA3oxl9X1"; -->
